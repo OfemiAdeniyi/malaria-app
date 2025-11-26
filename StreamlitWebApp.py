1cbc579b8650833fbc5031ad577f1f71a52a03fa
Micheal Oluwafemi Adeniyi <160871523+OfemiAdeniyi@users.noreply.github.com>
2025-11-27 00:08:41 +0100

    Update StreamlitWebApp.py

--- StreamlitWebApp.py
+++ StreamlitWebApp.py
@@ -52,7 +52,7 @@ st.markdown(
 
     /* Card style */
     .card {{
-        background: white;
+        background: grey;
         border-radius: 10px;
         padding: 14px;
         box-shadow: 0 1px 4px rgba(16,24,40,0.06);
